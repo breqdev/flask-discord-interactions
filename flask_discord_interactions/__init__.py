f7859d5e983904c3e895122be396ce89a1b34e71
Wesley Chalmers <wesleyjchalmers@gmail.com>
2021-04-02 00:35:38 -0400

    Re-add deprecated names as aliases, fix __all__

--- flask_discord_interactions/__init__.py
+++ flask_discord_interactions/__init__.py
@@ -5,19 +5,27 @@ from .discord import DiscordInteractions, DiscordInteractionsBlueprint
 from .response import Response, ResponseType
 
 
+# deprecated names
+InteractionResponse = Response
+InteractionContext = Context
+
+
 __all__ = [
-    SlashCommand,
-    SlashCommandSubgroup,
-    SlashCommandGroup,
-    Context,
-    CommandOptionType,
-    ChannelType,
-    Member,
-    User,
-    Role,
-    Channel,
-    DiscordInteractions,
-    DiscordInteractionsBlueprint,
-    Response,
-    ResponseType
+    "SlashCommand",
+    "SlashCommandSubgroup",
+    "SlashCommandGroup",
+    "Context",
+    "CommandOptionType",
+    "ChannelType",
+    "Member",
+    "User",
+    "Role",
+    "Channel",
+    "DiscordInteractions",
+    "DiscordInteractionsBlueprint",
+    "Response",
+    "ResponseType",
+
+    "InteractionResponse",
+    "InteractionContext"
 ]
