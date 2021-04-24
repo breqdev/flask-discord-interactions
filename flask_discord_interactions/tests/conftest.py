754f25f8032a6fbac4b9579eb462fecebe5c1996
Brooke Chalmers <breq@breq.dev>
2021-04-24 01:12:37 -0400

    Move fixtures to a separate file

---
+++ flask_discord_interactions/tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+
+from flask import Flask
+
+from flask_discord_interactions import (DiscordInteractions,
+                                        Client)
+
+@pytest.fixture(scope="module")
+def discord():
+    app = Flask(__name__)
+    return DiscordInteractions(app)
+
+@pytest.fixture(scope="module")
+def client(discord):
+    return Client(discord)
--- flask_discord_interactions/tests/test_ping.py
+++ flask_discord_interactions/tests/test_ping.py
@@ -1,20 +1,3 @@
-import pytest
-
-from flask import Flask
-
-from flask_discord_interactions import (DiscordInteractions,
-                                        Client)
-
-@pytest.fixture
-def discord():
-    app = Flask(__name__)
-    return DiscordInteractions(app)
-
-@pytest.fixture
-def client(discord):
-    return Client(discord)
-
-
 def test_ping(discord, client):
     @discord.command()
     def ping(ctx, pong: str = "pong"):
