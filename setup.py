67f1a918314b02ee9e352b563c7341d5e595fed4
Brooke Chalmers <breq@breq.dev>
2021-05-05 01:23:22 -0400

    Bump Version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as file:
 
 setup(
     name='Flask-Discord-Interactions',
-    version='0.1.8',
+    version='0.1.9',
     url='https://github.com/Breq16/flask-discord-interactions',
     author='Brooke Chalmers',
     author_email='breq@breq.dev',
