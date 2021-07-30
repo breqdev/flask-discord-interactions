87f4fb8926229e48afc395ad562dfbd7ac38541f
Brooke Chalmers <breq@breq.dev>
2021-07-29 20:21:36 -0400

    Add some examples for multi-worker deployments

---
+++ examples/multiworker/automatic_conf.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+sys.path.insert(1, "examples/multiworker")
+
+from automatic import discord
+
+def on_starting(server):
+    print("registering commands!")
+    discord.update_slash_commands(guild_id=os.environ["TESTING_GUILD"])
