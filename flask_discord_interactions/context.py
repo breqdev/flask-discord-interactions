512f515f44273622c34cb293453c03fdb3d04062
Brooke Chalmers <breq@breq.dev>
2021-08-08 18:42:44 -0400

    Documentation

--- flask_discord_interactions/context.py
+++ flask_discord_interactions/context.py
@@ -37,6 +37,19 @@ class ChannelType:
 
 
 class Permission:
+    """
+    An object representing a single permission overwrite.
+
+    ``Permission(role='1234')`` allows users with role ID 1234 to use the
+    command
+
+    ``Permission(user='5678')`` allows user ID 5678 to use the command
+
+    ``Permission(role='9012', allow=False)`` denies users with role ID 9012
+    from using the command
+    """
+
+
     def __init__(self, role=None, user=None, allow=True):
         if bool(role) == bool(user):
             raise ValueError("specify only one of role or user")
