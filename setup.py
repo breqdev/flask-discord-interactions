90d064fe11889147a8759e600c2765384b5ae69b
Brooke <breq@breq.dev>
2022-02-26 22:42:08 -0500

    v1.4.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as file:
 
 setup(
     name="Flask-Discord-Interactions",
-    version="1.3.0",
+    version="1.4.0",
     url="https://github.com/Breq16/flask-discord-interactions",
     author="Brooke Chalmers",
     author_email="breq@breq.dev",
