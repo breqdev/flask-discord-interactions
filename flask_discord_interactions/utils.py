8af813ae4a011f426e6aae9e8485de3ac19a708f
Brooke Chalmers <breq@breq.dev>
2022-08-08 18:38:08 -0400

    document static_or_instance

--- flask_discord_interactions/utils.py
+++ flask_discord_interactions/utils.py
@@ -2,6 +2,11 @@ import functools
 
 
 class static_or_instance(object):
+    """
+    Decorator that allows a function to be called as either a static method
+    or as an instance method (receiving the instance as the first argument).
+    """
+
     def __init__(self, func):
         self.func = func
 
