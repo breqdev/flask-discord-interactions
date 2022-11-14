05f872a031accd186428b0daaef2eb9da02e72f0
Brooke Chalmers <breq@breq.dev>
2022-11-14 10:43:02 -0500

    Make Message.encode() always return bytes

--- flask_discord_interactions/models/message.py
+++ flask_discord_interactions/models/message.py
@@ -206,8 +206,8 @@ class Message(LoadableDataclass):
 
         Returns
         -------
-        string
-            A string containing the message data (either JSON or multipart).
+        bytes
+            Bytes containing the message data (either JSON or multipart).
         string
             The mimetype of the message data.
         """
@@ -243,4 +243,4 @@ class Message(LoadableDataclass):
 
             return (multipart.to_string(), multipart.content_type)
         else:
-            return (payload_json, "application/json")
+            return (payload_json.encode("utf-8"), "application/json")
